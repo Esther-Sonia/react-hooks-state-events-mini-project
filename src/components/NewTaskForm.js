72e61578ee5ed1923bba61b101b2d15e2c3c210d
Esther-Sonia <esthersonia21@gmail.com>
2025-04-17 20:28:03 +0300

    implement new task component to add new tasks

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [taskText, setTaskText] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState(categories[0]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (taskText && selectedCategory) {
+      onTaskFormSubmit({ text: taskText, category: selectedCategory });
+      setTaskText("");
+      setSelectedCategory(categories[0]); 
+        }
+  };
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" value={taskText} onChange={(e) => setTaskText (e.target.value)} />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select 
+        name="category"
+        value={selectedCategory}
+        onChange={(e) => setSelectedCategory(e.target.value)}>
+      {categories
+            .filter((category) => category !== "All")
+            .map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
